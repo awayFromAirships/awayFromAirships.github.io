1d1d011179cca6e32f7c875d9e6fa65bec572497
bonter <sayansed111@gmail.com>
2024-10-26 20:35:17 +0300

    1729964117

--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@
 
 for file in articles/*.md; do
   filename="${file%.*}"
-  pandoc -o "$filename.html" "$file" --template=static/template.html
+  pandoc -o "$filename.html" "$file" --template=static/template.html --gladtex
 done
 
 echo "```" > "articles/index.md"
@@ -15,8 +15,8 @@ find articles/ -name "*.html" -type f -printf "%f\n" | while read file; do
 done
 
 
-pandoc -o articles/index.html articles/index.md --template=static/template.html
-pandoc -o index.html index.md --template=static/template.html
+pandoc -o articles/index.html articles/index.md --template=static/template.html --gladtex
+pandoc -o index.html index.md --template=static/template.html --gladtex
 
 git add .
 git commit -m "`date +%s`"
