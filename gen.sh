322214d8bf8ce890996f0b6b22c6601fee1e4c4f
bonter <sayansed111@gmail.com>
2024-10-27 15:09:15 +0200

    1730034555

--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@
 
 for file in articles/*.md; do
   filename="${file%.*}"
-  pandoc -o "$filename.html" "$file" --template=static/template.html --mathml
+  pandoc -o "$filename.html" "$file" --template=static/template.html --mathml --toc
 done
 
 echo "```" > "articles/index.md"
@@ -14,10 +14,11 @@ find articles/ -name "*.html" -type f -printf "%f\n" | while read file; do
   echo "* [${file%.html}]($file)" >> "articles/index.md"
 done
 
-
+# generates articles outside
 pandoc -o articles/index.html articles/index.md --template=static/template.html --mathml
 pandoc -o index.html index.md --template=static/template.html --mathml
 
+# pushes ALL changes to github, which then updates the website to include the changes
 git add .
 git commit -m "`date +%s`"
 git push
