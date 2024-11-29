e759a2de5f169bc4dd3682656e57f3e1ef415c93
bonter <sayansed111@gmail.com>
2024-11-29 17:49:55 +0200

    1732895395

--- gen.sh
+++ gen.sh
@@ -17,6 +17,7 @@ done
 # generates articles outside
 pandoc -o articles/index.html articles/index.md --template=static/template.html --mathml
 pandoc -o index.html index.md --template=static/template.html --mathml
+pandoc -o images.html images.md --template=static/template.html --mathml
 
 # pushes ALL changes to github, which then updates the website to include the changes
 git add .
--- images.md
+++ images.md
@@ -4,4 +4,16 @@ title: Images
 # Images
 Here's a catalogue of some images I've made.
 
+<main class="catalog">
+<article>
+<img src="static/images/alt_lssrAndBelarus.png" alt="1">
+<h3> Flag of Belaro-Daugava SSR</h3>
+<p>Flag of Daugava with an additional Belarusian stripe.</p>
+</article>
+<article>
+
+</article>
+</main>
+
+
 
